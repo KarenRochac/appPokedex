9860385e6b421135f9443c4b3fa1bc2b767c5ec3
KarenRochac <karen.rocha23@etec.sp.gov.br>
2022-07-28 14:51:39 -0300

    2 files modified

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PokemonService } from '../services/pokemon.service';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
 
-  constructor() {}
+  constructor(public pokemonService: PokemonService) {}
 
+  ngOnInit() {
+    this.pokemonService.buscarTodosPokemon().subscribe((dados) => {
+      console.log(dados);
+    });
+  }
 }
