68ac0443ce01f85b2c3cfb43c5d6779f10720cf1
KarenRochac <karen.rocha23@etec.sp.gov.br>
2022-08-04 15:03:22 -0300

    metodobuscapokemon

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -4,15 +4,34 @@ import { PokemonService } from '../services/pokemon.service';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page implements OnInit {
+  total = 0;
+  next = '';
+  previous = '';
+  listaPokemon = [];
 
   constructor(public pokemonService: PokemonService) {}
 
   ngOnInit() {
+    this.buscarPokemon();
+  }
+
+  async buscarPokemon() {
     this.pokemonService.buscarTodosPokemon().subscribe((dados) => {
+      this.total = dados['count'];
+      this.next = dados['next'];
+      this.previous = dados['previous'];
+      //this.listaPokemon = dados['results'];
       console.log(dados);
+      for (let pokemon of dados['results']) {
+        this.pokemonService
+          .buscarUmPokemon(pokemon.url)
+          .subscribe((dadosPokemom) => {
+            this.listaPokemon.push(dadosPokemom);
+          });
+      }
     });
   }
 }
